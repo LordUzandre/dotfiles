e705ef9ce60e91647c972ee10ab2c6d982b22bd4
joaen574 <joaen574@upp-118.ad.liu.se>
2026-04-21 09:34:52 +0200

    added install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Abort if anything goes wrong
+set -e
+
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# Make sure that we don''t show all files in 'dotfiles status'
+dotfiles config --local status.showUntrackedFiles no
+
+# Install vim-plug
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+    echo "Installerar vim-plug..."
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# remove script after succesful install
+if [-f $0]; then
+	rm "$0"
+fi
