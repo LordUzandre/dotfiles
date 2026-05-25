df2417328eb27b08d01ac842e6cb9b5f37b18e62
joaen574 <joaen574@student.liu.se>
2026-05-25 17:12:51 +0200

    fixed keymaps in nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,19 +8,22 @@ local map = vim.keymap.set
 
 -- F2 to rename symbol
 map("n", "<F2>", vim.lsp.buf.rename, { desc = "LSP Rename" })
--- Switch between 0 and ^
-map({ "n", "v", "o" }, "0", "^")
-map({ "n", "v", "o" }, "^", "0")
--- mark entire row
-map("n", "vv", "^vg_", { desc = "Mark entire row" })
+
+-- Switch between 0 and ^ (safely)
+map({ "n", "v", "o" }, "0", function()
+  return vim.v.count == 0 and "^" or "0"
+end, { expr = true, desc = "Go to first non-blank character (respects count)" })
+map({ "n", "v", "o" }, "^", "0", { desc = "go to absolute start of line" })
 
 map("n", "gg", "gg^", { desc = "jump to beginning of file" })
 map("n", "G", "G$", { desc = "jump to end of file" })
 
-map("n", "d", "_d", { desc = "delete rather than copy" })
-map("n", "D", "_D")
-map("n", "x", "_x", { desc = "use X to remove rather than cut" })
+map({ "n", "v" }, "d", '"_d', { desc = "delete rather than copy" })
+map({ "n", "v" }, "D", '"_D', { desc = "delete to end of line" })
+map("n", "x", '"_x', { desc = "use X to remove rather than cut" })
 map("n", "y", '"+y', { desc = "copy to clipboard" })
+
+vim.opt.clipboard = "unnamedplus"
 -- visual mode
-map("v", "y", "+y")
-map("n", "Y", "+Y")
+-- map("v", "y", "+y")
+-- map("n", "Y", "+Y")
